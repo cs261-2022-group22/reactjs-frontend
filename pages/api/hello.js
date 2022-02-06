f179121f0cc4761940fc7fcd770eaa7f6bf4bb82
Varun Chodanker <57418924+varunchodanker@users.noreply.github.com>
2022-02-06 00:56:10 +0000

    GRPC implemented for authentication. .proto file added for this

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-    res.status(200).json({ name: "John Doe" });
+export default async function handler(req, res) {
+    return new Promise(function(authResolve, authReject) {
+        setTimeout(() => {
+            res.status(200).json({ name: "John Doe" });
+            authResolve();
+        }, 3000);
+    });
+    // setTimeout(() => {
+    //     res.status(200).json({ name: "John Doe" });
+    // }, 10000);
 }
