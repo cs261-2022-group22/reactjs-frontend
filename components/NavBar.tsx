d84cbc12734009e4fe0d96c1c83fe9d17c7849b6
Kiran Sanganee <sanganee12@gmail.com>
2022-02-13 16:52:33 +0000

    basic navbar complete

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,23 @@
-import { AppBar } from "@mui/material";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import Link from "next/link";
+import HelpIcon from "@mui/icons-material/Help";
 
 function NavBar() {
-	return (
-		<AppBar>
-			Test
-		</AppBar>
-	)
+    return (
+        <AppBar position="static">
+			<Box sx={{ flexGrow: 1 }}>
+				<Toolbar>
+					<Typography variant="h6">Logo</Typography>
+					<Typography sx={{ flexGrow: 1, textAlign: "center" }} variant="h5">
+						Website Name
+					</Typography>
+					<Link href="/tutorial">
+						<Button color="inherit" endIcon={<HelpIcon/>}>Help</Button>						
+					</Link>
+				</Toolbar>
+			</Box>
+        </AppBar>
+    );
 }
 
-export default NavBar
+export default NavBar;
---
+++ pages/tutorial.tsx
@@ -0,0 +1,9 @@
+function Tutorial() {
+	return (
+		<div>
+			The tutorial page
+		</div>
+	)
+}
+
+export default Tutorial
