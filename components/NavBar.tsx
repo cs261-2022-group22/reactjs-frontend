c3397d89c8de7ea674177f5ab121d9d1e6ce3f19
Kiran Sanganee <sanganee12@gmail.com>
2022-02-15 17:19:35 +0000

    fixed alignment

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, Grid, Toolbar, Typography } from "@mui/material";
 import Link from "next/link";
 import HelpIcon from "@mui/icons-material/Help";
 
@@ -7,18 +7,27 @@ function NavBar() {
         <AppBar position="static">
             <Box sx={{ flexGrow: 1 }}>
                 <Toolbar>
-                    <Typography variant="h6">Logo</Typography>
-                    <Typography
-                        sx={{ flexGrow: 1, textAlign: "center" }}
-                        variant="h5"
-                    >
-                        Website Name
-                    </Typography>
-                    <Link href="/tutorial">
-                        <Button color="inherit" endIcon={<HelpIcon />}>
-                            Help
-                        </Button>
-                    </Link>
+                    <Grid container spacing={12}>
+                        <Grid item xs={2}>
+                            <Typography variant="h6">Logo</Typography>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <Typography
+                                sx={{ flexGrow: 1, textAlign: "center" }}
+                                variant="h5"
+                            >
+                                Website Name
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={1}></Grid>
+                        <Grid item xs={1}>
+                            <Link href="/tutorial">
+                                <Button color="inherit" endIcon={<HelpIcon />}>
+                                    Help
+                                </Button>
+                            </Link>
+                        </Grid>
+                    </Grid>
                 </Toolbar>
             </Box>
         </AppBar>
