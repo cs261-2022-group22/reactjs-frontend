3ee24bb5ee9a3872e7f0107851b616e022a16b75
David <DavidDJackman@gmail.com>
2022-02-15 20:22:07 +0000

    components + layout

---
+++ components/FrontPagePicture.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+function FrontPagePicture() {
+  return (
+    <Card sx={{ maxWidth: 1000, maxHeight: 1000, mt: 5, mb: 5 }}>
+      <CardActionArea style={{ justifyContent: "center", alignItems: "center",display: "flex" }}>
+        <CardMedia  sx={{maxWidth: 1000, maxHeight: 1000}}
+          component="img"
+          image="/Images/Mentor.jpg"
+          
+          alt="Mentor"
+        />
+
+      </CardActionArea>
+    </Card>
+  );
+}
+export default FrontPagePicture
