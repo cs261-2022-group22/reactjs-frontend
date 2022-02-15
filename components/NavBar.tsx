ee9af898994814d041b8cb590ed921092a4c50cd
Kiran Sanganee <sanganee12@gmail.com>
2022-02-15 20:45:48 +0000

    navbar now scales with width

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,31 +5,31 @@ import HelpIcon from "@mui/icons-material/Help";
 function NavBar() {
     return (
         <AppBar position="static">
-            <Box sx={{ flexGrow: 1 }}>
-                <Toolbar>
-                    <Grid container spacing={12}>
-                        <Grid item xs={2}>
-                            <Typography variant="h6">Logo</Typography>
-                        </Grid>
-                        <Grid item xs={8}>
-                            <Typography
-                                sx={{ flexGrow: 1, textAlign: "center" }}
-                                variant="h5"
-                            >
-                                Website Name
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={1}></Grid>
-                        <Grid item xs={1}>
-                            <Link href="/tutorial">
-                                <Button color="inherit" endIcon={<HelpIcon />}>
-                                    Help
-                                </Button>
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </Toolbar>
-            </Box>
+			<Toolbar>
+				<Grid container>
+					<Grid item xs={2} sx={{ display: "flex", justifyContent: "flex-start"}}>
+						<Typography variant="h6">Logo</Typography>
+					</Grid>
+					<Grid item xs={8}>
+						<Typography
+							sx={{ textAlign: "center" }}
+							variant="h5"
+						>
+							Website Name
+						</Typography>
+					</Grid>
+					<Grid item xs={2} sx={{ display: "flex", justifyContent: "flex-end"}}>
+						<Link href="/tutorial">
+							<Button
+								color="inherit"
+								endIcon={<HelpIcon />}
+							>
+								Help
+							</Button>
+						</Link>
+					</Grid>
+				</Grid>
+			</Toolbar>
         </AppBar>
     );
 }
