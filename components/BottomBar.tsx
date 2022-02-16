14c470a67aa939f84afd7485fdea89a1fc3384c1
Moody <mooodyhunter@outlook.com>
2022-02-16 16:07:43 +0000

    Navbar (#2)
    * added mui, fixed mini bugs
    * made navbar component file
    * basic navbar complete
    * basic bottom bar component
    * temp deverloper feedback page
    * fixed alignment
    * added hover change to icon in bottom bar
    * navbar now scales with width
    
    Co-authored-by: Kiran Sanganee <sanganee12@gmail.com>

---
+++ components/BottomBar.tsx
@@ -0,0 +1,30 @@
+import {
+    AppBar,
+    BottomNavigation,
+    BottomNavigationAction,
+} from "@mui/material";
+import FeedbackIcon from "@mui/icons-material/Feedback";
+import Link from "next/link";
+
+function BottomBar() {
+    return (
+        <AppBar
+            position="fixed"
+            color="primary"
+            style={{ top: "auto", bottom: 0 }}
+        >
+            <BottomNavigation>
+                <Link href="/developerFeedback">
+                    <BottomNavigationAction
+                        label="Feedback"
+                        icon={<FeedbackIcon />}
+                        showLabel
+                        sx={{ "&:hover": { color: "#5d6166" } }}
+                    />
+                </Link>
+            </BottomNavigation>
+        </AppBar>
+    );
+}
+
+export default BottomBar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,29 @@
 import LoginButton from "components/LoginButton";
 import NavBar from "components/NavBar";
+import BottomBar from "components/BottomBar";
 import { Grid } from "@mui/material";
 import FrontPagePicture from "components/FrontPagePicture";
 import RegisterButton from "components/RegisterButton";
 import FrontPageText from "components/FrontPageText";
 
 function Home() {
-    return (
+	return (
 		<><NavBar />
-		<Grid container spacing={5} alignItems="center" justifyContent="space-evenly">
-		
-			<Grid item style={{ justifyContent: "center", display: "flex" }} xs={11}>
-				<FrontPagePicture />
-				
+			<Grid container spacing={5} alignItems="center" justifyContent="space-evenly">
+				<Grid item style={{ justifyContent: "center", display: "flex" }} xs={11}>
+					<FrontPagePicture />
+				</Grid>
+				<Grid item style={{ justifyContent: "right", display: "flex" }} xs={6} >
+					<LoginButton />
+				</Grid>
+				<Grid item style={{ justifyContent: "left", display: "flex" }} xs={6}>
+					<RegisterButton />
+				</Grid>
+				<Grid item style={{ justifyContent: "center", display: "flex" }} xs={10} >
+					<FrontPageText />
+				</Grid>
 			</Grid>
-			<Grid item style={{ justifyContent: "right", display: "flex" }} xs={6} >
-				<LoginButton />
-				
-				
-			</Grid>
-			<Grid item style={{ justifyContent: "left", display: "flex" }} xs={6}>
-				<RegisterButton />
-				
-				
-			</Grid>
-			<Grid item style={{ justifyContent: "center", display: "flex" }} xs={10} >
-				<FrontPageText />
-				
-				
-			</Grid>
-
-		
-		</Grid>
+			<BottomBar />
 		</>
 
 	)
