66d18cde52a5e9e2e4157564404c755e37048681
Kiran Sanganee <sanganee12@gmail.com>
2022-03-02 16:06:39 +0000

    Mentee links (#37)
    * Added links section
    * Added links section
    * Added pages
    * Added pages
    * Changed name to MenteeLinks
    
    Co-authored-by: Xieyu Wang Lin <ezukillu@gmail.com>
    Co-authored-by: Moody <mooodyhunter@outlook.com>

---
+++ components/MenteeLinks.tsx
@@ -0,0 +1,59 @@
+import { Button, Grid, Typography } from '@mui/material';
+import Link from "next/link";
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+function MenteeLinks() {
+    return (
+        <Grid container rowSpacing={5} sx={{ textAlign: "center", mt: 1, p: 1 }}>
+            <Grid item xs={6}>           
+                <Link href="/mentee/schedule" passHref>
+                    <Button 
+                        sx={{ 
+                            flexDirection: 'column', 
+                            height: 100,
+                            width: 150
+                        }}>
+                        <AddBoxIcon />
+                        <Typography variant="h5" sx={{ textTransform: 'capitalize' }}>
+                            Schedule a meeting
+                        </Typography>
+                    </Button> 
+                </Link>              
+            </Grid>
+            <Grid item xs={6}>
+                <Link href="/mentee/planofactions" passHref>
+                        <Button 
+                            sx={{ 
+                                flexDirection: 'column', 
+                                height: 100,
+                                width: 150
+                            }}>
+                            <AssignmentIcon />
+                            <Typography variant="h5" sx={{ textTransform: 'capitalize' }}>
+                                Plan of Actions
+                            </Typography>
+                        </Button>                        
+                </Link>
+            </Grid>
+            <Grid item xs={12}>
+                <Link href="/mentee/details" passHref>
+                    <Button 
+                        sx={{ 
+                            flexDirection: 'column', 
+                            height: 100,
+                            width: 150
+                        }}>
+                        <SettingsIcon />
+                        <Typography variant="h5" sx={{ textTransform: 'capitalize' }}>
+                            Change details
+                        </Typography>
+                    </Button>  
+                </Link> 
+            </Grid>
+        </Grid>
+        
+    );
+}
+export default MenteeLinks;
--- pages/mentee/dashboard.tsx
+++ pages/mentee/dashboard.tsx
@@ -1,12 +1,13 @@
 import { Grid } from "@mui/material";
+import MenteeLinks from  "components/MenteeLinks";
 import UpcomingAppointments from "components/UpcomingAppointments";
 
 export default function MenteeDashboard() {
     return (
         <Grid container>
-            <Grid container item xs={12} sx={{ height: "46vh" }}>
+            <Grid container item xs={12} sx={{ height: "48vh"}}>
                 <Grid item xs={6}>
-                    Top left
+                    <MenteeLinks/>
                 </Grid>
                 <Grid item xs={6}>
                     <UpcomingAppointments cancellable={true}/>
