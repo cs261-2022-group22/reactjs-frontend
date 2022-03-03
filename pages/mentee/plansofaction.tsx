6073b2dd4a29eabfefabd23d3ff688c445fd5ad5
Kiran Sanganee <sanganee12@gmail.com>
2022-03-03 19:01:06 +0000

    partially done

--- pages/mentee/plansofaction.tsx
+++ pages/mentee/plansofaction.tsx
@@ -1,7 +1,84 @@
+import {
+    Container,
+    Stack,
+    TextField,
+    Typography,
+	Button,
+    List,
+    Card,
+	Grid,
+} from "@mui/material";
+import { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+
+const test_data = ["Wake up", "Eat breakfast", "Shower"];
+
 export default function planofactions() {
+    const [userInput, setUserInput] = useState("");
+	const [elements, setElements] = useState(test_data)
+
     return (
         <>
-            <div>The Plan of Actions page</div>
+            <Container sx={{ textAlign: "center" }}>
+                <Stack>
+                    <Typography variant="h3">Your plan of action</Typography>
+                    <TextField
+                        sx={{
+                            mb: "2vh",
+                        }}
+                        label="Enter your milestones:"
+                        onChange={(e) => {
+                            setUserInput(e.target.value);
+                        }}
+                    />
+                    <Grid container justifyContent="center">
+                        <Button
+                            variant="contained"
+                            endIcon={<AddIcon />}
+                            sx={{
+                                minWidth: "11vh",
+                            }}
+                            onClick={() => {
+                                setElements([...elements, userInput]);
+                            }}
+                        >
+                            Add
+                        </Button>
+                    </Grid>
+                    <List
+                        sx={{
+                            textAlign: "left",
+                        }}
+                    >
+                        {elements.map((element) => {
+                            return (
+                                <Card
+                                    sx={{
+                                        mb: "2vh",
+                                        pl: "2vh",
+                                        height: "6vh",
+                                        boxShadow: 1,
+                                        "&:hover": { boxShadow: 3 },
+                                        display: "flex",
+                                        flexDirection: "column",
+                                        justifyContent: "center",
+                                    }}
+                                    key={element}
+                                >
+                                    <Typography
+                                        sx={{
+                                            fontWeight: "bold",
+                                        }}
+                                    >
+                                        {element}
+                                    </Typography>
+                                </Card>
+                            );
+                        })}
+                    </List>
+                </Stack>
+                {userInput}
+            </Container>
         </>
     );
 }
