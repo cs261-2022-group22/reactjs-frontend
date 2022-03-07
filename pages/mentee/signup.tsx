4924a5b7df77dba5d1f2689263ced0efce565729
Kiran Sanganee <sanganee12@gmail.com>
2022-03-07 19:48:04 +0000

    basic page, multi select added, buttons too small, need rpc

--- pages/mentee/signup.tsx
+++ pages/mentee/signup.tsx
@@ -1,7 +1,88 @@
+import { Box, Chip, Container, FormControl, InputLabel, MenuItem, OutlinedInput, Select, SelectChangeEvent, Stack, Typography, Button } from "@mui/material";
+import { useState } from "react";
+
+const skills = [
+    "Technical",
+    "How To Progress Career",
+    "Management",
+    "Leadership",
+    "Healthy Work-Life balance"
+];
+
 export default function MenteeSignUp() {
+	const [skillState, setskillState] = useState<string[]>([]);
+
+    const handleChange = (event: SelectChangeEvent<typeof skillState>) => {
+        const {
+            target: { value },
+        } = event;
+        setskillState(
+            typeof value === "string" ? value.split(",") : value
+        );
+    };
     return (
         <>
-            <div>The mentee sign up page</div>
+            <Container sx={{ textAlign: "center" }}>
+                <Stack
+                    direction="column"
+                >
+                    <Typography sx={{ mt: "5vh", mb: "5vh" }} variant="h3">
+                        Mentee Registration
+                    </Typography>
+                    <FormControl>
+                        <InputLabel>Skill</InputLabel>
+                        <Select
+                            multiple
+                            value={skillState}
+                            onChange={handleChange}
+                            input={<OutlinedInput label="Skill" />}
+                            sx={{
+                                minHeight: "8vh",
+                            }}
+                            renderValue={(selected) => (
+                                <Box
+                                    sx={{
+                                        display: "flex",
+                                        flexWrap: "wrap",
+                                        gap: 0.5,
+                                    }}
+                                >
+                                    {selected.map((value) => (
+                                        <Chip key={value} label={value} />
+                                    ))}
+                                </Box>
+                            )}
+                        >
+                            {skills.map((skill) => (
+                                <MenuItem key={skill} value={skill}>
+                                    {skill}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+					<Stack sx={{
+                        direction: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}>
+						<Box
+							sx={{
+								display: "grid",
+								rowGap: 2,
+								mt: "10vh",
+								maxWidth: "20vh",
+							}}
+						>
+							<Button variant="contained" onClick={() => {
+								console.log(skillState)
+							}}>Register</Button>
+							<Button variant="outlined" color="error" href="/">
+								Cancel
+							</Button>
+						</Box>
+					</Stack>
+                </Stack>
+            </Container>
         </>
     );
 }
