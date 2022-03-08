144f19f3f46c9dbd8e019ef502793b42d1e5df02
Kiran Sanganee <sanganee12@gmail.com>
2022-03-08 17:21:09 +0000

    mentee registration working

--- pages/api/user/registermentee.ts
+++ pages/api/user/registermentee.ts
@@ -3,18 +3,12 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { AccountClient } from "utils/rpcClients";
 
 export default async function registermentee(req: NextApiRequest, res: NextApiResponse) {
-	console.log("got to api")
-	console.log(req, req.body);
-
-    const client = new AccountClient();
-
     try {
+		const client = new AccountClient();
         const result = await client.registerMenteeAsync({
 			userid: req.body.userid,
 			desiredSkills: req.body.desiredSkills,
         });
-
-        console.log(result);
 
         res.status(200).json({status: result.status});
     } catch (error) {
--- pages/mentee/signup.tsx
+++ pages/mentee/signup.tsx
@@ -11,7 +11,8 @@ import {
     Stack,
     Typography,
     Button,
-	Link
+    Link,
+	Alert,
 } from "@mui/material";
 import axios from "axios";
 import Unauthenticated from "components/Unauthenticated";
@@ -31,8 +32,8 @@ const skills = [
 export default function MenteeSignUp() {
     const [skillState, setskillState] = useState<string[]>([]);
     const [displayRequired, setDisplayRequired] = useState(false);
-	// 3 status': normal, false, success
-	const [status, setStatus] = useState("normal")
+    // 3 status': normal, false, success
+    const [status, setStatus] = useState("normal");
     const { data: session } = useSession();
     if (!session) {
         return <Unauthenticated />;
@@ -44,128 +45,140 @@ export default function MenteeSignUp() {
         } = event;
         setskillState(typeof value === "string" ? value.split(",") : value);
     };
-	if (status == "normal") {
-		return (
-			<>
-				<Container sx={{ textAlign: "center" }}>
-					<Stack direction="column">
-						<Typography sx={{ mt: "5vh", mb: "5vh" }} variant="h3">
-							Mentee Registration
-						</Typography>
-						<FormControl>
-							<InputLabel>Skill</InputLabel>
-							<Select
-								multiple
-								value={skillState}
-								onChange={handleChange}
-								input={<OutlinedInput label="Skill" />}
-								sx={{
-									minHeight: "8vh",
-								}}
-								renderValue={(selected) => (
-									<Box
-										sx={{
-											display: "flex",
-											flexWrap: "wrap",
-											gap: 0.5,
-										}}
-									>
-										{selected.map((value) => (
-											<Chip key={value} label={value} />
-										))}
-									</Box>
-								)}
-							>
-								{skills.map((skill) => (
-									<MenuItem key={skill} value={skill}>
-										{skill}
-									</MenuItem>
-								))}
-							</Select>
-						</FormControl>
-						<Stack
-							sx={{
-								direction: "column",
-								alignItems: "center",
-								justifyContent: "center",
-							}}
-						>
-							{displayRequired ? (
-								<Typography>
-									Please add at least one skill
-								</Typography>
-							) : (
-								<></>
-							)}
-							<Box
-								sx={{
-									display: "grid",
-									rowGap: 2,
-									mt: "10vh",
-									maxWidth: "20vh",
-								}}
-							>
-								<Button
-									variant="contained"
-									onClick={async () => {
-										console.log(skillState);
-										if (skillState.length <= 0) {
-											setDisplayRequired(true);
-										} else {
-											const res = await axios.post("/api/user/registermentee", {
-												userid: session["id"] as number,
-												desiredSkills: skillState
-											})
-											if (res.data.status) {
-												setStatus("success");
-											} else {
-												setStatus("false");
-											}											
-										}
-									}}
-								>
-									Register
-								</Button>
-								<Button variant="outlined" color="error" href="/">
-									Cancel
-								</Button>
-							</Box>
-						</Stack>
-					</Stack>
-				</Container>
-			</>
-		);
-	} else if (status == "false") {
-		<Container sx={{ textAlign: "center" }}>
-            <Link href="/mentee/signup">
-                <Typography variant="h3">
+    if (status == "normal") {
+        return (
+            <>
+                <Container sx={{ textAlign: "center" }}>
+                    <Stack direction="column">
+                        <Typography sx={{ mt: "5vh", mb: "5vh" }} variant="h3">
+                            Mentee Registration
+                        </Typography>
+                        <FormControl>
+                            <InputLabel>Skill</InputLabel>
+                            <Select
+                                multiple
+                                value={skillState}
+                                onChange={handleChange}
+                                input={<OutlinedInput label="Skill" />}
+                                sx={{
+                                    minHeight: "8vh",
+                                }}
+                                renderValue={(selected) => (
+                                    <Box
+                                        sx={{
+                                            display: "flex",
+                                            flexWrap: "wrap",
+                                            gap: 0.5,
+                                        }}
+                                    >
+                                        {selected.map((value) => (
+                                            <Chip key={value} label={value} />
+                                        ))}
+                                    </Box>
+                                )}
+                            >
+                                {skills.map((skill) => (
+                                    <MenuItem key={skill} value={skill}>
+                                        {skill}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                        <Stack
+                            sx={{
+                                direction: "column",
+                                alignItems: "center",
+                                justifyContent: "center",
+                            }}
+                        >
+                            {displayRequired ? (
+                                <Typography>
+                                    Please add at least one skill
+                                </Typography>
+                            ) : (
+                                <></>
+                            )}
+                            <Box
+                                sx={{
+                                    display: "grid",
+                                    rowGap: 2,
+                                    mt: "10vh",
+                                    maxWidth: "20vh",
+                                }}
+                            >
+                                <Button
+                                    variant="contained"
+                                    onClick={async () => {
+                                        if (skillState.length <= 0) {
+                                            setDisplayRequired(true);
+                                        } else {
+                                            const res = await axios.post(
+                                                "/api/user/registermentee",
+                                                {
+                                                    userid: session[
+                                                        "id"
+                                                    ] as number,
+                                                    desiredSkills: skillState,
+                                                }
+                                            );
+                                            if (res.data.status) {
+                                                setStatus("success");
+                                            } else {
+                                                setStatus("false");
+                                            }
+                                        }
+                                    }}
+                                >
+                                    Register
+                                </Button>
+                                <Button
+                                    variant="outlined"
+                                    color="error"
+                                    href="/"
+                                >
+                                    Cancel
+                                </Button>
+                            </Box>
+                        </Stack>
+                    </Stack>
+                </Container>
+            </>
+        );
+    } else if (status == "false") {
+        return (
+            <Container sx={{ textAlign: "center" }}>
+                <Alert severity="warning" sx={{ mt: "3vh", mb: "3vh" }}>
                     An error has occurred, please try again (You may already
-                    have an acccount)
-                </Typography>
-                <Button
-                    variant="contained"
-                    size="large"
-                    startIcon={<RefreshIcon />}
-                >
-                    Try again
-                </Button>
-            </Link>
-        </Container>;
-	} else {
-		<Container sx={{ textAlign: "center" }}>
-            <Link href="/mentee/dashboard">
-                <Typography variant="h3">
+                    have an account)
+                </Alert>
+                <Link href="/mentee/signup">
+                    <Button
+                        variant="contained"
+                        size="large"
+                        startIcon={<RefreshIcon />}
+                    >
+                        Try again
+                    </Button>
+                </Link>
+            </Container>
+        );
+    } else {
+        return (
+            <Container sx={{ textAlign: "center" }}>
+                <Alert severity="info" sx={{ mt: "3vh", mb: "3vh"}}>
                     You have successfully signed up. Click below to take you to
                     the dashboard
-                </Typography>
-                <Button
-                    variant="contained"
-                    size="large"
-                    startIcon={<ArrowForwardIcon />}
-                >
-                    Go to dashboard
-                </Button>
-            </Link>
-        </Container>;
-	}
+                </Alert>
+                <Link href="/mentee/dashboard">
+                    <Button
+                        variant="contained"
+                        size="large"
+                        startIcon={<ArrowForwardIcon />}
+                    >
+                        Go to dashboard
+                    </Button>
+                </Link>
+            </Container>
+        );
+    }
 }
