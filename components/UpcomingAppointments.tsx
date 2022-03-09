d9420617aff0c8444c24e01c59d066cebe3d983f
Kiran Sanganee <sanganee12@gmail.com>
2022-03-09 17:22:03 +0000

    fixed key bug

--- components/UpcomingAppointments.tsx
+++ components/UpcomingAppointments.tsx
@@ -1,11 +1,17 @@
-import { Cancel as CancelIcon, Refresh as RefreshIcon } from "@mui/icons-material";
+import {
+    Cancel as CancelIcon,
+    Refresh as RefreshIcon,
+} from "@mui/icons-material";
 import { Card, List, ListItem, Paper, Stack, Typography } from "@mui/material";
 import groupBy from "lodash.groupby";
 import Link from "next/link";
 import { useState } from "react";
 import { NormalisedAppointment } from "utils/CommonTypes";
 
-export default function UpcomingAppointments(props: { appointments: NormalisedAppointment[]; cancellable: boolean; }) {
+export default function UpcomingAppointments(props: {
+    appointments: NormalisedAppointment[];
+    cancellable: boolean;
+}) {
     const ga = groupBy(props.appointments, "date");
     const d = [];
     for (const key in ga) {
@@ -23,45 +29,125 @@ export default function UpcomingAppointments(props: { appointments: NormalisedAp
     }
 
     return (
-        <Paper variant='outlined' sx={{ padding: '10px', mt: '15px', boxShadow: 0, maxWidth: "100vh", backgroundColor: "transparent" }} elevation={0} >
-            <Stack direction="row" justifyContent="space-between" sx={{ mb: '5px' }}>
+        <Paper
+            variant="outlined"
+            sx={{
+                padding: "10px",
+                mt: "15px",
+                boxShadow: 0,
+                maxWidth: "100vh",
+                backgroundColor: "transparent",
+            }}
+            elevation={0}
+        >
+            <Stack
+                direction="row"
+                justifyContent="space-between"
+                sx={{ mb: "5px" }}
+            >
                 <Typography variant="h5">Upcoming Appointments</Typography>
-                <RefreshIcon sx={{ "&:hover": { color: "blue" } }} onClick={update} />
+                <RefreshIcon
+                    sx={{ "&:hover": { color: "blue" } }}
+                    onClick={update}
+                />
             </Stack>
-            <List sx={{ maxHeight: "37vh", overflowY: 'scroll' }}>
+            <List sx={{ maxHeight: "37vh", overflowY: "scroll" }}>
                 {dates.map((date) => (
-                    <Card sx={{ pr: '5px', pl: '8px', ml: '5px', mr: '5px', mt: "10px", boxShadow: 1, "&:hover": { boxShadow: 4 }, backgroundColor: "#fafafa" }} key={date}>
-                        <Typography sx={{ fontWeight: "bold", mb: "-1vh", pt: "6px" }}>{date}</Typography>
+                    <Card
+                        sx={{
+                            pr: "5px",
+                            pl: "8px",
+                            ml: "5px",
+                            mr: "5px",
+                            mt: "10px",
+                            boxShadow: 1,
+                            "&:hover": { boxShadow: 4 },
+                            backgroundColor: "#fafafa",
+                        }}
+                        key={date}
+                    >
+                        <Typography
+                            sx={{ fontWeight: "bold", mb: "-1vh", pt: "6px" }}
+                        >
+                            {date}
+                        </Typography>
                         <List>
-                            {groupedAppointments[date].map(
-                                (appointment) => {
+                            {groupedAppointments[date].map((appointment) => {
                                 if (appointment.type == 0) {
-                                  return (
-                                    <Link href={appointment.link} passHref>
-                                      <ListItem sx={{ mb: "-1vh" }} key={appointment.date + appointment.time}>
-                                        <Typography sx={{ fontWeight: "bold", mb: "-1px" }}>{appointment.time}</Typography>:
-                                        Meeting lasting {appointment.duration} minutes
-                                        {props.cancellable && <CancelIcon sx={{ color: "red", marginLeft: "auto" }} />}
-                                      </ListItem>
-                                    </Link>
-                                  )
+                                    return (
+                                        <Link
+                                            href={appointment.link}
+                                            key={
+                                                appointment.date +
+                                                appointment.time
+                                            }
+                                            passHref
+                                        >
+                                            <ListItem
+                                                sx={{ mb: "-1vh" }}
+                                            >
+                                                <Typography
+                                                    sx={{
+                                                        fontWeight: "bold",
+                                                        mb: "-1px",
+                                                    }}
+                                                >
+                                                    {appointment.time}
+                                                </Typography>
+                                                : Meeting lasting{" "}
+                                                {appointment.duration} minutes
+                                                {props.cancellable && (
+                                                    <CancelIcon
+                                                        sx={{
+                                                            color: "red",
+                                                            marginLeft: "auto",
+                                                        }}
+                                                    />
+                                                )}
+                                            </ListItem>
+                                        </Link>
+                                    );
                                 } else {
-                                  return (
-                                    <Link href={appointment.link} passHref>
-                                      <ListItem sx={{ mb: "-1vh" }} key={appointment.date + appointment.time}>
-                                        <Typography sx={{ fontWeight: "bold", mb: "-1px" }}>{appointment.time}</Typography>:
-                                        Workshop on {appointment.skill} lasting {appointment.duration} minutes
-                                        {props.cancellable && <CancelIcon sx={{ color: "red", marginLeft: "auto" }} />}
-                                      </ListItem>
-                                    </Link>
-                                  )
+                                    return (
+                                        <Link
+                                            href={appointment.link}
+                                            key={
+                                                appointment.date +
+                                                appointment.time
+                                            }
+                                            passHref
+                                        >
+                                            <ListItem
+                                                sx={{ mb: "-1vh" }}
+                                            >
+                                                <Typography
+                                                    sx={{
+                                                        fontWeight: "bold",
+                                                        mb: "-1px",
+                                                    }}
+                                                >
+                                                    {appointment.time}
+                                                </Typography>
+                                                : Workshop on{" "}
+                                                {appointment.skill} lasting{" "}
+                                                {appointment.duration} minutes
+                                                {props.cancellable && (
+                                                    <CancelIcon
+                                                        sx={{
+                                                            color: "red",
+                                                            marginLeft: "auto",
+                                                        }}
+                                                    />
+                                                )}
+                                            </ListItem>
+                                        </Link>
+                                    );
                                 }
-                              }
-                            )}
+                            })}
                         </List>
                     </Card>
                 ))}
             </List>
-        </Paper >
-	);
+        </Paper>
+    );
 }
