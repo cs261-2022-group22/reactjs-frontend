ab8e35c2e958011dd1683102deb6dd599e6bb3cb
Kiran Sanganee <sanganee12@gmail.com>
2022-03-14 00:52:51 +0000

    Update devfeedback.ts

--- pages/api/user/devfeedback.ts
+++ pages/api/user/devfeedback.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { FeedbackClient } from "utils/rpcClients";
 import { getSession } from "next-auth/react";
 
-export default async function ChangeDetails(req: NextApiRequest, res: NextApiResponse) {
+export default async function developerfeedback(req: NextApiRequest, res: NextApiResponse) {
 	const session = await getSession({ req });
     if (!session) {
         res.status(403).json({ error: "Not logged in", success: false });
